e010a6134ccd1cf1263ed12c9e0d3cb0e365be98
Jonny Alves <jonnyccalves@gmail.com>
2025-02-04 15:41:42 -0300

    adicionando operações no controller usando as operações da entidade do panacheEntity

--- src/main/java/br/com/rest/UserController.java
+++ src/main/java/br/com/rest/UserController.java
@@ -2,6 +2,9 @@ package br.com.rest;
 
 import br.com.domain.User;
 import br.com.rest.request.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -12,6 +15,7 @@ import jakarta.ws.rs.core.Response;
 public class UserController {
 
     @POST
+    @Transactional
     public Response createUser(CreateUserRequest userRequest){
         User user = new User();
         user.setName(userRequest.getName());
@@ -24,6 +28,36 @@ public class UserController {
 
     @GET
     public Response listAllUsers(){
-        return Response.ok().build();
+        PanacheQuery<User> allUsers = User.findAll();
+
+        return Response.ok(allUsers.list()).build();
+    }
+
+    @DELETE
+    @Transactional
+    @Path("{id}")
+    public Response deleteUser(@PathParam("id") Long id){
+        User user = User.findById(id);
+
+        if(user != null){
+            user.delete();
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userRequest){
+        User user = User.findById(id);
+
+        if(user != null){
+            user.setAge(userRequest.getAge());
+            user.setName(userRequest.getName());
+            return Response.ok(user).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
